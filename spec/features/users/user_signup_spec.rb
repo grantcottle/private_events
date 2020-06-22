cd216ade6e3a12dcd9933b9f5c70cd9101f6605f
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-06-22 09:43:44 -0700

    adding simple form

---
+++ spec/features/users/user_signup_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "User signup", type: :feature do
+  let(:user) { build(:user) }
+
+  scenario 'user can sign up with valid data' do
+    visit new_user_registration_path
+
+    fill_in 'user_username', with: user.username
+    page.attach_file("user_avatar", Rails.root + 'app/javascript/images/round-profile-image-placeholder.png') 
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    fill_in 'user_password_confirmation', with: user.password_confirmation
+
+    expect do
+      click_button 'Sign up'
+    end.to change(User, :count).by(1)
+  end
+end
