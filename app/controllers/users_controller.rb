51bb05bed9309e37723b0c3cc202376f44e7bc20
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-06-22 09:42:15 -0700

    styling user forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,40 @@
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :authenticate_user!, only: [:edit]
   def index
-    @users = User.all    
+    @users = User.all
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }   
+      else
+        format.html {render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_path, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :avatar)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -4,10 +4,19 @@ require 'rails_helper'
 
 describe 'Users', type: :request do
   describe 'GET index' do
-    let(:user) { FactoryBot.create(:user) }
+    let!(:user) { FactoryBot.create(:user) }
     it 'renders :index template' do
-      get users_path 
-      expect(response).to be_successful 
+      get users_path
+      expect(response).to be_successful
+      expect(response.body).to include(user.username)
+    end
+  end
+
+  describe 'GET show' do
+    let!(:user) { FactoryBot.create(:user) }
+    it 'renders :show template' do
+      get user_path(user)
+      expect(response).to be_successful
       expect(response.body).to include(user.username)
     end
   end
