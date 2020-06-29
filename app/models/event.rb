54cf64c9f14321291219907e9d0df7a7ca257c3e
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-06-29 13:16:38 -0700

    adding event edit link, debugging image upload for seeding db

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :invitations, foreign_key: "attended_event_id"
+  has_many :invitations, foreign_key: "attended_event_id", dependent: :destroy
   has_many :attendess, through: :invitations, source: :attendee
-  has_one_attached :cover_image
+  has_one_attached :cover_image, dependent: :destroy
   scope :past_events, -> {where("date < ?", Date.today).order("date DESC")}
   scope :live_events, -> {where("date >= ?", Date.today).order("date ASC")}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :avatar
-  has_many :events, foreign_key: 'creator_id'
-  has_many :invitations, foreign_key: 'attendee_id'
+  has_one_attached :avatar, dependent: :destroy
+  has_many :events, foreign_key: 'creator_id', dependent: :destroy
+  has_many :invitations, foreign_key: 'attendee_id', dependent: :destroy
   has_many :attended_events, through: :invitations
 
   def past_events
