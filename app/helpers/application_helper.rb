18576a8dd62607d5199d938f513ff3e8fa09a1ce
Grant Cottle <grantcottle@Grants-MacBook-Pro.local>
2020-07-01 12:48:48 -0700

    fixing cover image form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
     if event.cover_image.attached?
       event.cover_image.variant(resize_to_limit: [width, height])
     else
-      ActionController::Base.helpers.image_url('placeholder.jpg', size: size)
+      ActionController::Base.helpers.image_url('placeholder.jpg', width: width, height: height)
     end
   end
 end
